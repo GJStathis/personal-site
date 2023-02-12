ead5bd6d4246d70279aa01422eecae9ca6598eb4
Gregory Stathis <gstathis22@gmail.com>
2023-02-12 17:23:06 -0500

    - added media queries for the blog posts - Added animations for the hamburger menu

--- components/hamburger.tsx
+++ components/hamburger.tsx
@@ -1,34 +1,52 @@
-// import { useState } from 'react'
-// import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import styles from "../styles/HamburgerMenu.module.css"
-// import Link from 'next/link'
+import { useState, useRef, useEffect } from 'react'
+import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from "../styles/HamburgerMenu.module.css"
+import Link from 'next/link'
 
-// function HamburgerMenu() {
-//     const [navbarOpen, setNavbarOpen] = useState(false);
+function HamburgerMenu() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const hamburgerRef = useRef(null);
 
-//     return(
-//         <nav className={styles.navBar}>
-//             <div className={styles.navBarButton} onClick={() => setNavbarOpen(!navbarOpen)}>
-//                 {navbarOpen ?
-//                     (<FontAwesomeIcon icon={faCircleXmark} size={"2x"} />)
-//                 :
-//                     (<FontAwesomeIcon icon={faBars} size={"2x"} />)
-//                 }
-//             </div>
-//             <ul className={`${styles.navMenu} ${navbarOpen ? styles.navOpen: ''}`}>
-//                 <li>
-//                     <Link href='/'>Home</Link>
-//                 </li>
-//                 <li>
-//                     <Link href='/blog'>Blog</Link>
-//                 </li>
-//                 <li>
-//                     <Link href='/projects'>Projects</Link>
-//                 </li>
-//             </ul>
-//         </nav>
-//     )
-// }
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, false);
+        return () => {
+            document.removeEventListener("click", handleClickOutside, false);
+        };
+    })
 
-// export default HamburgerMenu
+    const handleClickOutside = (event) => {
+        if (hamburgerRef.current && navbarOpen && !hamburgerRef.current.contains(event.target)) {
+            setNavbarOpen(prev => !prev)
+        }
+    }
+  
+    return(
+        <div ref={hamburgerRef}>
+            <nav className={styles.navBar}>
+                <div className={styles.navBarButton} onClick={() => setNavbarOpen(!navbarOpen)}>
+                    {navbarOpen ?
+                        (<FontAwesomeIcon icon={faCircleXmark} size={"2x"} />)
+                    :
+                        (<FontAwesomeIcon icon={faBars} size={"2x"} />)
+                    }
+                </div>
+                <div className={`${styles.navMenu} ${navbarOpen ? styles.navOpen : styles.navClosed}`}>
+                    <ul className={styles.navList}>
+                        <li>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link href='/blog'>Blog</Link>
+                        </li>
+                        <li>
+                            <Link href='/projects'>Projects</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default HamburgerMenu
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import Navbar from '../components/navbar'
+import HamburgerMenu from '../components/hamburger'
 import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Navbar />
+      <HamburgerMenu/>
       <Component {...pageProps} />
     </SessionProvider>
   )
